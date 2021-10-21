74782ce3a65ea907b310bcfbc02660f181d1a91d
mahela98 <chandimaofficial@gmail.com>
2021-10-21 23:16:06 +0530

    problem when loading the image --have to use selenium

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const { find } = require('domutils');
+// const { response } = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 4000;
@@ -12,29 +14,72 @@ const websites = [
     },
 ]
 const movies = []
+const movieUrls = [
+    {
+        name: 'yts',
+        address: 'https://yts.rs/movie/the-velvet-underground-2021',
+        base: 'https://yts.rs',
+    }
+]
+const movieDetails = []
 
-websites.forEach(website => {
-    axios.get(website.address)
+// websites.forEach(website => {
+//     axios.get(website.address)
+//         .then(response => {
+//             const html = response.data;
+//             const $ = cheerio.load(html)
+//             $('.text--bold.palewhite.title', html).each(function () {
+//                 const title = $(this).text()
+//                 const url = $(this).attr('href')
+//                 movies.push({
+//                     title,
+//                     url: website.base + url,
+//                     source: website.name
+//                 })
+//                 // console.log(title);
+//             })
+//         }).catch(err => console.log(err))
+// });
+
+movieUrls.forEach(async movieUrl => {
+   await axios.get(movieUrl.address)
         .then(response => {
-            const html = response.data
-            const $ = cheerio.load(html)
-            // console.log(html)
-            $('.text--bold.palewhite.title', html).each(function () {
-                const title = $(this).text()
-                const url = $(this).attr('href')
-                movies.push({
-                    title,
-                    url: website.base + url,
-                    source: website.name
-                })
-                console.log(title);
-            })
+            const html = response.data;
+            const $ = cheerio.load(html);
+            var title;
+            var rYear;
+            var movieType;
+            var imageUrl;
+            var IMDBRate;
+            var torrentUrls = [];
 
-        }).catch(err => console.log(err))
+            title = $('div[class="visible-xs col-xs-20 movie-info text--white"]', html).find('h1').first().text();
+            rYear = $('div[class="visible-xs col-xs-20 movie-info text--white"]', html).find('h2').first().text();
+            movieType= $('div[class="visible-xs col-xs-20 movie-info text--white"]', html).find('h2').eq(1).text();
+            IMDBRate= $('span[itemprop="ratingValue"]', html).text();
+            // var imageAttr= $('img[class="hero__card-img image"]', html).attr();
+            // imageUrl= (imageAttr === undefined) ? null : imageAttr.src;
+            imageUrl= $('div[class="image-container"]', html).find('img').attr('alt');
+            console.log(rYear);
+            console.log(title);
+            console.log(movieType);
+            console.log(IMDBRate);
+            console.log(imageUrl);
+
+
+        }).catch(err => console.log("error"))
 })
 
+
+
+
+
+
+
+
+
 app.get('/', (req, res) => {
-   
+
     res.json('Welcome to my Climate Change News API')
 })
 
