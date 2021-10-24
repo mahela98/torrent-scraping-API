974da55fd811800a43f722970da2606a29cae57a
mahela98 <chandimaofficial@gmail.com>
2021-10-24 11:32:50 +0530

    puppeteer gets movie urls imge urls and name

--- torrentSearch.js
+++ torrentSearch.js
@@ -10,6 +10,37 @@ const torrentSearch = async (searchQuery) => {
 
     await page.waitForSelector("div[class=__header]");
     await page.screenshot({ path: 'example.png' });
+    // const parent = await page.$eval('div[class=card]', result => result);
+    // const title = parent.querySelector('a[class=image-container-link popup123] ').href;
+    const heading1 = await page.$eval("h2[class='text--green']", el => el.textContent);
+    console.log(heading1);
+
+    // await page.evaluate(() => {
+    //     let results = [];
+    //     let items = document.querySelectorAll('div[class=card]');
+    //     items.forEach((item) => {
+    //         console.log('asd')
+    //     })
+
+    // })
+
+    const movieUrls = await page.evaluate(() => Array
+        .from(document.querySelectorAll("div[class=card] > a[class='text--bold palewhite title']"), element => element.href));
+    const movieName = await page.evaluate(() => Array
+        .from(document.querySelectorAll("div[class=card] > a[class='text--bold palewhite title']"), element => element.innerText));
+        const movieImage = await page.evaluate(() => Array
+        .from(document.querySelectorAll("div[class=card] > a[class='image-container-link popup123'] > img[class='hero__card-img image']"), element => element.src));
+
+    // text.forEach((x) => {
+    //     console.log(x)
+    // })
+for (let index = 0; index < movieName.length; index++) {
+   console.log(movieName[index])
+   console.log(movieImage[index])
+    
+}
+
+
 
     await browser.close();
 };
